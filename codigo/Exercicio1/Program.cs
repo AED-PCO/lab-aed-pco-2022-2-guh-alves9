8bb0677ef00f070ae876479f4c6ee6ae4a2bcc42
guh-alves9 <gustavo-teixeira2002@hotmail.com>
2022-08-28 20:10:40 -0300

    commit

---
+++ codigo/Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+               for(int x = 1000; x <= 9999; x++){
+
+               String primeiraMetade = Convert.ToString(x).Substring(0,2);
+               string segundaMetade = Convert.ToString(x).Substring(2);
+
+               int soma = int.Parse(primeiraMetade) + int.Parse(segundaMetade);
+
+                     if(Math.Pow(soma,2)== x){
+                     System.Console.WriteLine(x);
+                     }      
+               }
+        }
+
+    }
+     
+}
